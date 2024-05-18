4ecffbda32ee226113c7ffb52ea593d809c5f163
Aman Upadhyay <amanupadhyay33822@gmail.com>
2024-05-19 01:56:21 +0530

    j

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
-const age= 24;
+const fs = require("fs");
+
+
+
+// fs.writeFileSync("dummy.txt","hello world")
+
+// const data=fs.readFile("dummy.txt",(err, data) => {
+//     console.log(data.toString());
+// });
+console.log(__filename);
+
+fs.readdir(".../",(err, data) => {
+    console.log(data);
+})
+
+// __dirname repesent the current directory
